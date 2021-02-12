8c15ef98bee43e519ebe28ed6e88914c461c55ad
MikevPeeren <MikevPeeren@hotmail.com>
2021-02-12 14:26:48 +0100

    :recycle: added constants and adjust responsiveness

--- constants/general.ts
+++ constants/general.ts
@@ -1,15 +1,7 @@
-export const HEADER = 'Mike van Peeren - Full Stack Developer';
-
 export const META_TITLE = 'Mike van Peeren - Full Stack Developer, Tilburg - Working @ Blink';
 export const META_DESCRIPTION =
-  'Mike van Peeren MERN Stack, JS, ESM, TS, React, NodeJS, Next.js, SQL, NoSQL, WebPack, Babel, CSS, SASS, TailwindCSS, HTML5, GCD, Jest, GitLab, CI/CD';
+  'Mike van Peeren Full(MERN) Stack Developer, JavaScript, TypeScript, React, Node.js, Next.js, Sass, TailwindCSS, HTML5, GCD, Jest, GitLab';
 
-export const TITLE = 'Full Stack Engineer 💻 - NodeJS Enthusiast 💡';
-export const LOCATION = 'Tilburg, Noord Brabant - 51.560596° N, 5.0919143° E';
-export const GET_IN_TOUCH = 'Get In Touch';
+export const HEADER = 'Mike van Peeren - Full Stack Developer';
 
-export const FIRST_PARAGRAPH = `My name is Mike van Peeren a Full Stack Engineer.
-                I love creating applications that have a social impact on the world of it's consumers. This provides a
-                sense of meaningfulness for me in my work and pushes me to deliver results as close to perfection as
-                possible. My drive for perfection also pushes me to keep myself up to date about new innovations and
-                developments in my line of work and to always learn from colleagues and the big WorldWideWeb.`;
+export const SUBHEADER = 'Node.js & React Enthusiast💡, Full Stack Developer 💻 and Loving Father.';
--- pages/components/EntryPage.tsx
+++ pages/components/EntryPage.tsx
@@ -4,25 +4,26 @@ import React, { ReactElement, FC } from 'react';
 // Components
 import SocialMedia from './SocialMedia';
 
+// Constants
+import { HEADER, SUBHEADER } from '../../constants/general.ts';
+
 const EntryPage: FC = (): ReactElement => {
   return (
     <div className="mt-20">
       <div className="flex flex-col lg:flex-row">
         <div className="flex flex-col justify-center order-1 w-full lg:w-3/5">
-          <h1 className="text-5xl tracking-wider mb-2 mx-auto lg:-ml-0">
-            I&apos;m <span className="font-bold">Mike van Peeren</span>
+          <h1 className="text-5xl tracking-wider mb-2 mx-auto md:-ml-0">
+            I&apos;m <span className="font-bold">{HEADER}</span>
           </h1>
-          <h4 className="text-2xl mx-auto lg:-ml-0">
-            Node.js & React Enthusiast, Full Stack Developer and Loving Father.
-          </h4>
+          <h4 className="text-2xl mx-auto md:-ml-0">{SUBHEADER}</h4>
 
-          <div className="flex flex-row order-1 lg:w-3/5 mt-8 mx-auto lg:-ml-0">
+          <div className="flex flex-row order-1 lg:w-3/5 mt-8 mx-auto md:-ml-0">
             <SocialMedia />
           </div>
         </div>
         <div className="flex flex-row lg:justify-end order-2 w-full mt-8 lg:mt-0 lg:w-2/5 mb-8">
           <img
-            className="lg:w-10/12 w-8/12 lg:mr-0 rounded-xl shadow-xl mx-auto lg:-ml-0"
+            className="lg:w-10/12 w-8/12 lg:mr-0 rounded-xl shadow-xl mx-auto md:-ml-0"
             src="https://www.mikevpeeren.nl/mikevpeeren.webp"
           />
         </div>
