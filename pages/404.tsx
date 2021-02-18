21b1ccab48ddb3ccc081b29fda98c941f8f06a38
MikevPeeren <MikevPeeren@hotmail.com>
2021-02-18 11:58:17 +0100

    :sparkles: added custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+// React
+import React, { ReactElement, FC } from 'react';
+
+const FourOhFour: FC = (): ReactElement => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto text-center">
+        <h1 className="text-9xl text-gray-600 font-bold">404</h1>
+        <h2 className="text-2xl text-gray-600 font-bold">Not Found</h2>
+        <p className="mt-8">The resource requested could not be found on this server!</p>
+      </div>
+    </div>
+  );
+};
+
+export default FourOhFour;
