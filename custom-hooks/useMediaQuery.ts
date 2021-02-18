757dec3c58c9eb2f6bb68617ed2c78c0827c92e9
MikevPeeren <MikevPeeren@hotmail.com>
2021-02-18 09:11:10 +0100

    :bug: adjustment to typing

--- custom-hooks/useMediaQuery.ts
+++ custom-hooks/useMediaQuery.ts
@@ -3,10 +3,10 @@ import { useState, useEffect } from 'react';
 
 /**
  * Custom hook to determine the current window so we can fiddle with it in our component
- * @param {string} query
+ * @param {string | boolean} query
  */
-export function useMediaQuery(query: string): string {
-  const [matches, setMatches] = useState('');
+export function useMediaQuery(query: string | boolean): string {
+  const [matches, setMatches] = useState(false);
 
   useEffect(() => {
     const media = window.matchMedia(query);
