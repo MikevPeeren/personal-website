78bdbb03eea01e51f9f8d31569f762354ea6339e
MikevPeeren <MikevPeeren@hotmail.com>
2021-02-25 20:39:41 +0100

    :zap: improving font load

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,8 @@ const Home: FC = (): ReactElement => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="font"
           crossOrigin="anonymous"
         />
         <link rel="canonical" href={canURL} />
