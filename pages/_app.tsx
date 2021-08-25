dd2ae84c3fb58962d8fffbcca2afc962dbecb129
MikevPeeren <MikevPeeren@hotmail.com>
2021-08-25 15:21:41 +0200

    :bug: removing unused param

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ import { META_TITLE, META_TITLE_ALTERNATIVE } from '../constants/general';
 
 const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
   useEffect(() => {
-    document.addEventListener('visibilitychange', (event) => {
+    document.addEventListener('visibilitychange', () => {
       if (document.visibilityState == 'visible') document.title = META_TITLE;
       else document.title = META_TITLE_ALTERNATIVE;
     });
