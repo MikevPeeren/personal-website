8ce5ca32280d41d8f582980d3440f759c3b9ca81
MikevPeeren <MikevPeeren@hotmail.com>
2021-08-25 09:30:56 +0200

    :recycle: updating meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,12 +43,12 @@ const Home: FC = (): ReactElement => {
     <div ref={homeRef}>
       <Head>
         <title>{META_TITLE}</title>
-        <link rel="icon" href="./favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="icon" href="meta/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="meta/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="meta/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="meta/favicon-16x16.png" />
+        <link rel="manifest" href="meta/site.webmanifest" />
+        <link rel="mask-icon" href="meta/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
