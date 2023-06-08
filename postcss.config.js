aab1e72be2ec7554d2299c8e823dc44eb8e93596
MikevPeeren <mikevpeeren@hotmail.com>
2023-06-08 06:20:34 +0200

    chore: added cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env", "autoprefixer"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+  },
 };
