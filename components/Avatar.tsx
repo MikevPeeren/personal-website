21021e8fb34dd0894d46779519b94d3a7cff1781
MikevPeeren <mikevpeeren@hotmail.com>
2024-06-03 09:35:04 +0200

    fix: improvements for lighthouse

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -17,8 +17,8 @@ const Avatar = ({
         width: "100%",
         height: "50px",
       }}
-      width={500}
-      height={300}
+      width={100}
+      height={60}
       className="rounded-full border-2"
     />
   );
