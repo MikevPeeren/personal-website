5572a09108c5040bf0de5415877eac38433044de
MikevPeeren <mikevpeeren@hotmail.com>
2024-06-04 06:54:22 +0200

    chore: changing sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,31 @@
+import { MetadataRoute } from "next";
 
-import { compareDesc } from "date-fns";
-
-import { posts } from '@/.velite';
+import { posts } from "@/.velite";
 
 const URL = "https://mikevpeeren.nl";
 
-export default async function sitemap() {
-  const sortedAllPosts = posts.sort((a, b) =>
-    compareDesc(new Date(a.publishDate), new Date(b.publishDate))
-  );
-
+export default function sitemap(): MetadataRoute.Sitemap {
   const allPosts = posts.map(({ slug, publishDate }) => ({
     url: `${URL}/blog/${slug}`,
     lastModified: publishDate,
     changeFrequency: "daily",
+    priority: 0.8,
   }));
 
-  const routes = ["", "/blog","/projects","/about","/contact"].map((route) => ({
+  const dailyRoutes = ["/blog"].map((route) => ({
     url: `${URL}${route}`,
     lastModified: new Date().toISOString(),
     changeFrequency: "daily",
+    priority: 0.5,
   }));
 
-  return [...routes, ...allPosts];
+  const otherRoutes = ["", "/projects", "/about", "/contact"].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "weekly",
+    priority: 1,
+  }));
+
+  // @ts-expect-error changeFrequency is correct
+  return [...otherRoutes, ...dailyRoutes, ...allPosts];
 }
