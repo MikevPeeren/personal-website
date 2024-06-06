ddc1fb777502156fd0a9e7d2ea59a9083c702c6f
MikevPeeren <mikevpeeren@hotmail.com>
2024-06-06 10:14:10 +0200

    fix: changing stroke-width

--- components/icons/MediumIcon.tsx
+++ components/icons/MediumIcon.tsx
@@ -20,7 +20,7 @@ const MediumIcon = ({
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 483 483"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
         strokeLinecap="round"
