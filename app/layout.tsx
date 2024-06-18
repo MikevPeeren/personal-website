9b28acf5b861be2cc5bcb2c84bc358743a381d82
MikevPeeren <mikevpeeren@hotmail.com>
2024-06-18 14:19:40 +0200

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Footer from "@/components/Footer";
@@ -88,6 +89,7 @@ export default function RootLayout({
             <PageWrapper>
               <Navigation />
               <div className="h-full w-full pb-20">{children}</div>
+              <Analytics />
               <SpeedInsights />
             </PageWrapper>
             <Footer />
